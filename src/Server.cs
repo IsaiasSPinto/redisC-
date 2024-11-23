97be8b7acb20b997c84050ed7317990267eff049
Isaias Pinto <isaiascxs10@gmail.com>
2024-11-23 11:48:42 -0300

    Feat : Handle multiple clients

--- src/Server.cs
+++ src/Server.cs
@@ -2,18 +2,26 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-var client = server.AcceptSocket(); // wait for client
 
-while (client.Connected)
+
+while (true)
 {
-    var buffer = new byte[1024];
-    await client.ReceiveAsync(buffer);
-    await client.SendAsync(Encoding.ASCII.GetBytes("+PONG\r\n"));
+    var client = await server.AcceptSocketAsync();
+    _ = HandleClientAsync(client);
+}
+
+
+async static Task HandleClientAsync(Socket client)
+{
+    while (client.Connected)
+    {
+        var buffer = new byte[1024];
+        await client.ReceiveAsync(buffer);
+        await client.SendAsync(Encoding.ASCII.GetBytes("+PONG\r\n"));
+    }
 }
 
